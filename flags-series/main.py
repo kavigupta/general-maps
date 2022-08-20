d6462c469db91eac02b58ace021c0c17832cd2bd
Kavi Gupta <kavi@berkeley.edu>
2022-08-20 04:57:36 -0400

    add script

---
+++ flags-series/main.py
@@ -0,0 +1,147 @@
+import datetime
+import os
+import subprocess
+import tempfile
+
+import gspread
+import tweepy
+
+import pandas as pd
+
+from secret import *
+
+NAME = "Flagration"
+
+ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
+
+GITHUB_PATH = "https://github.com/kavigupta/general-maps/blob/master/flags-series/"
+
+TIME_TO_PUBLISH = "13:00"
+
+
+def render_image(file_name):
+    png_path = tempfile.mktemp()
+
+    subprocess.check_call(
+        [
+            "inkscape",
+            os.path.join(ROOT_PATH, "flags", file_name),
+            "-e",
+            png_path,
+            "-w",
+            "4096",
+        ]
+    )
+    return png_path
+
+
+def kavis_tweet(id):
+    return f"https://twitter.com/notkavi/status/{id}"
+
+
+def tweet_image(tweets_table, api, *, png_path, index, name, description):
+    reply_to = tweet_to_reply_to(tweets_table, index)
+    status = f"{NAME} Episode {index}: {name}\n\n{description}"
+    main_tweet = api.update_with_media(png_path, status=status)
+    thread_tweet = api.update_status(
+        f"{NAME} Episode {index}: {name}\n" + kavis_tweet(main_tweet.id),
+        in_reply_to_status_id=reply_to,
+    )
+    return main_tweet.id, thread_tweet.id
+
+
+def tweet_to_reply_to(tweets_table, index):
+    above = tweets_table[tweets_table["Index"] == str(int(index) - 1)]["Thread id"]
+    assert len(above) == 1
+    return above.iloc[0]
+
+
+def read_tweets_table(tweets_sheet):
+    header, *values = tweets_sheet.get_all_values()
+    return pd.DataFrame(values, columns=header)
+
+
+def update_tweets_table(tweets_sheet, *, update_index, main_tweet, thread_tweet):
+    tweets_table = read_tweets_table(tweets_sheet)
+
+    [idx] = tweets_table[tweets_table["Index"] == update_index].index
+    row = idx + 2
+    tweets_sheet.update_cell(row, list(tweets_table).index("Shipped?") + 1, "TRUE")
+    tweets_sheet.update_cell(
+        row, list(tweets_table).index("Main id") + 1, str(main_tweet)
+    )
+    tweets_sheet.update_cell(
+        row, list(tweets_table).index("Thread id") + 1, str(thread_tweet)
+    )
+
+
+def get_time_to_publish(row):
+    return datetime.datetime.strptime(
+        row["Date"] + " " + TIME_TO_PUBLISH, "%Y-%m-%d %H:%M"
+    )
+
+
+def run_for_row(twitter_api, tweets_sheet, row):
+    if datetime.datetime.now() - get_time_to_publish(row) <= datetime.timedelta(0):
+        return
+    png_path = render_image(row["Flag path"])
+
+    index, name, description = row["Index"], row["Flag name"], row["Flag description"]
+
+    main_tweet, thread_tweet = tweet_image(
+        read_tweets_table(tweets_sheet),
+        twitter_api,
+        png_path=png_path,
+        index=index,
+        name=name,
+        description=description,
+    )
+
+    update_tweets_table(
+        tweets_sheet,
+        update_index=index,
+        main_tweet=main_tweet,
+        thread_tweet=thread_tweet,
+    )
+
+    full_list = gspread.service_account().open("Full list of maps and flags")
+    full_list.sheet1.append_row(
+        [
+            f"{NAME} Episode {index}: {name}",
+            int(index),
+            row["Date"],
+            "Flag redesign",
+            NAME,
+            GITHUB_PATH + row["Flag path"],
+            kavis_tweet(main_tweet),
+        ]
+    )
+
+
+def gather_row(tweets_table):
+    to_ship = tweets_table[
+        (tweets_table["Ship?"] == "TRUE") & (tweets_table["Shipped?"] == "FALSE")
+    ]
+    if len(to_ship) == 0:
+        return None
+    index = min((idx for idx in to_ship["Index"]), key=int)
+    row = to_ship[to_ship["Index"] == index].iloc[0]
+    return row
+
+
+def main():
+    tweets_sheet = gspread.service_account().open(NAME).sheet1
+    row = gather_row(read_tweets_table(tweets_sheet))
+
+    if row is None:
+        return
+
+    my_auth = tweepy.OAuthHandler(my_consumer_key, my_consumer_secret)
+    my_auth.set_access_token(my_access_token, my_access_token_secret)
+    twitter_api = tweepy.API(my_auth)
+
+    run_for_row(twitter_api, tweets_sheet, row)
+
+
+if __name__ == "__main__":
+    main()
