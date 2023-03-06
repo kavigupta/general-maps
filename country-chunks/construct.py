eba399850ab2a7bff08b19f84401bd3fb57e45f8
Kavi Gupta <kavi@berkeley.edu>
2023-03-05 19:22:32 -0500

    updates

--- country-chunks/construct.py
+++ country-chunks/construct.py
@@ -1,5 +1,6 @@
 from functools import lru_cache
 
+import us
 import subprocess
 import numpy as np
 import pandas as pd
@@ -11,6 +12,8 @@ import wquantiles
 from permacache import permacache, drop_if_equal
 
 terr = {"78", "72", "69", "66", "60", "11"}
+
+counts = {'Alabama': '7', 'Alaska': '1', 'Arizona': '9', 'Arkansas': '4', 'California': '52', 'Colorado': '8', 'Connecticut': '5', 'Delaware': '1', 'Florida': '28', 'Georgia': '14', 'Hawaii': '2', 'Idaho': '2', 'Illinois': '17', 'Indiana': '9', 'Iowa': '4', 'Kansas': '4', 'Kentucky': '6', 'Louisiana': '6', 'Maine': '2', 'Maryland': '8', 'Massachusetts': '9', 'Michigan': '13', 'Minnesota': '8', 'Mississippi': '4', 'Missouri': '8', 'Montana': '2', 'Nebraska': '3', 'Nevada': '4', 'New Hampshire': '2', 'New Jersey': '12', 'New Mexico': '3', 'New York': '26', 'North Carolina': '14', 'North Dakota': '1', 'Ohio': '15', 'Oklahoma': '5', 'Oregon': '6', 'Pennsylvania': '17', 'Rhode Island': '2', 'South Carolina': '7', 'South Dakota': '1', 'Tennessee': '9', 'Texas': '38', 'Utah': '4', 'Vermont': '1', 'Virginia': '11', 'Washington': '10', 'West Virginia': '2', 'Wisconsin': '8', 'Wyoming': '1'}
 
 
 def state(x):
@@ -145,8 +148,10 @@ def centroids():
     return pd.read_csv("/home/kavi/full-usa-precinct-map/out/centroids.csv")
 
 
-def margin(chunk):
+def margin(chunk, state_filter=None):
     res = centroids()
+    if state_filter is not None:
+        res = res[res.state == us.states.lookup(state_filter).abbr]
     minx, miny, maxx, maxy = chunk.bounds
     votes = res[(minx < res.x) & (res.x < maxx) & (miny < res.y) & (res.y < maxy)][
         ["R", "D", "O"]
@@ -155,7 +160,7 @@ def margin(chunk):
     return 0 if np.isnan(result) else result
 
 
-@permacache("construct/produce_map_2")
+@permacache("construct/produce_map_3")
 def produce_map(amount, seed, state_filter=None):
     us = usa()
     if state_filter is not None:
@@ -175,7 +180,7 @@ def produce_map(amount, seed, state_filter=None):
     )
     pops = [x.POP100.sum() for x in tables]
     np.max(pops) / np.min(pops)
-    margins = [margin(c) for c in chunks]
+    margins = [margin(c, state_filter=state_filter) for c in chunks]
     chunks = [res_up(chunk) for chunk in tqdm.tqdm(chunks)]
     frame = gpd.GeoDataFrame(
         {"id": [x + 1 for x in range(len(chunks))], "margin": margins}, geometry=chunks
@@ -183,8 +188,10 @@ def produce_map(amount, seed, state_filter=None):
     return frame
 
 
-def export_map(amount, plan, *, prefix, layout, **kwargs):
+def export_map(amount, plan, *, prefix, layout, no_img=False, **kwargs):
     frame = produce_map(amount, plan, **kwargs)
+    if no_img:
+        return frame
     plan = str(plan)
     margins = np.array(frame.margin)
     biden, trump = (np.array(margins) > 0).sum(), (np.array(margins) < 0).sum()
@@ -194,9 +201,10 @@ def export_map(amount, plan, *, prefix, layout, **kwargs):
         if median_district > 0
         else f"R+{-median_district:.2f}"
     )
-    frame.to_file("temp/temp.shp")
+    frame.to_file("/home/kavi/temp/temp.shp")
     with open(f"out/{amount}_{plan}.geojson", "w") as f:
         f.write(frame.to_json())
+        
     for typ in layout:
         print(amount, plan, typ)
         subprocess.check_call(
@@ -209,7 +217,32 @@ def export_map(amount, plan, *, prefix, layout, **kwargs):
                 f"out/{prefix}{typ}_{amount}_{plan}.png",
             ]
         )
+        
+    return frame
 
+def standard(state, n_d, plan=1, **kwargs):
+    fips = us.states.lookup(state).fips
+    return export_map(
+        n_d,
+        plan,
+        prefix=f"{state}_",
+        state_filter=fips,
+        layout=dict(partisanship=f"Partisanship {state.upper()}", atlas=f"Main {state.upper()}"),
+        **kwargs,
+    )
+
+@permacache("construct/margins")
+def margins(state, n_d, plan):
+    fips = us.states.lookup(state).fips
+    x = export_map(
+        n_d,
+        plan,
+        prefix=f"{state}_",
+        state_filter=fips,
+        layout=dict(partisanship=f"Partisanship {state.upper()}", atlas=f"Main {state.upper()}"),
+        no_img=True,
+    ).margin
+    return [float(x) for x in x]
 
 def main(mode):
     if mode == "usa":
@@ -243,7 +276,35 @@ def main(mode):
                 state_filter="36",
                 layout=dict(partisanship="Partisanship NY", atlas="Main NY"),
             )
-
+    elif mode == "ca":
+        for plan in range(1, 3):
+            n_d = 400 if plan == 1 else 3098
+            export_map(
+                n_d,
+                plan,
+                prefix="ca_",
+                state_filter="06",
+                layout=dict(partisanship="Partisanship CA", atlas="Main CA"),
+            )
+    elif mode == "wy":
+        standard(mode, 175)
+    elif mode == "sd":
+        standard(mode, 240)
+    elif mode == "congressional_test":
+        results = {}
+        for plan in range(1000):
+            results[plan] = {}
+            for state in us.states.STATES:
+                print(state, plan)
+                mode = state.abbr.lower()
+                x = margins(mode, int(counts[us.states.lookup(mode).name]), plan=plan)
+                for i in range(len(x)):
+                    results[plan][f"{state.abbr}-{i + 1}"] = x[i]
+            with open("out/congressional_test.json", "w") as f:
+                import json
+                json.dump(results, f)
+    else:
+        standard(mode, 400)
 
 if __name__ == "__main__":
-    main("ny")
+    main("congressional_test")
